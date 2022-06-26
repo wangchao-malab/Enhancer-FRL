d4a2a5f016e4150f21f58a4a8931cc42016bb9bc
wangchao-malab <37856514+wangchao-malab@users.noreply.github.com>
2022-06-26 13:54:31 +0800

    Add files via upload

---
+++ Enhancer-FRL_master/feature_descriptors/descnucleotide/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+__all__ = [
+    'Kmer',
+    'ACC',
+    'ANF',
+    'ENAC',
+    'CKSNAP',
+    'NCP',
+    'Pse',
+    'check_parameters',
+    'PseEIIP',
+]
